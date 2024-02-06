ef4720e2003adbbf0771c457c7c8489f0daa62d5
joaoLouceiro <jrlouceiro@live.com>
2024-02-06 22:44:37 +0000

    Remove NumberPad component
    Need to add C and CE functionalities
    Will add some stylings, maybe with Tailwind

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import { useState } from "react";
 import Display from "./components/Display";
-import NumberPad from "./components/NumberPad";
 import Operation from "./components/Operation";
+import Number from "./components/Number";
 
 export default function Calculator() {
   const [display, setDisplay] = useState(0);
-  const [numArr, setNumArr] = useState([0]);
   const [main, setMain] = useState({
     a: [0],
     b: [0],
@@ -28,11 +27,12 @@ export default function Calculator() {
     return num1 * num2;
   }
 
-  function handleNumberClick(num) {
+  function handleClick(num) {
     const next = { ...main };
-    next.b = num;
+    if (next.b[0] === 0) next.b.shift();
+    next.b.push(num);
     console.log(next);
-    setDisplay(num);
+    setDisplay(next.b);
     setMain(next);
   }
 
@@ -64,19 +64,28 @@ export default function Calculator() {
     <main className="calculator">
       <Display value={display} />
       <div style={{ display: "flex" }}>
-        <div style={{ marginRight: "0.3rem" }}>
-          <NumberPad
-            clickedNumbers={main.b}
-            onNumberPadClick={handleNumberClick}
-            handleEquals={handleEquals}
-          />
-        </div>
-        <div>
-          <Operation value={"+"} onClickOperation={() => handleOp(add)} />
-          <Operation value={"-"} onClickOperation={() => handleOp(sub)} />
-          <Operation value={"x"} onClickOperation={() => handleOp(mult)} />
-          <Operation value={"/"} onClickOperation={() => handleOp(divide)} />
-        </div>
+        <Number value="1" onNumberClick={() => handleClick(1)} />
+        <Number value="2" onNumberClick={() => handleClick(2)} />
+        <Number value="3" onNumberClick={() => handleClick(3)} />
+        <Operation value={"+"} onClickOperation={() => handleOp(add)} />
+      </div>
+      <div style={{ display: "flex" }}>
+        <Number value="4" onNumberClick={() => handleClick(4)} />
+        <Number value="5" onNumberClick={() => handleClick(5)} />
+        <Number value="6" onNumberClick={() => handleClick(6)} />
+        <Operation value={"-"} onClickOperation={() => handleOp(sub)} />
+      </div>
+      <div style={{ display: "flex" }}>
+        <Number value="7" onNumberClick={() => handleClick(7)} />
+        <Number value="8" onNumberClick={() => handleClick(8)} />
+        <Number value="9" onNumberClick={() => handleClick(9)} />
+        <Operation value={"x"} onClickOperation={() => handleOp(mult)} />
+      </div>
+      <div style={{ display: "flex" }}>
+        <Number value="." onNumberClick={() => handleClick(".")} />
+        <Number value="0" onNumberClick={() => handleClick(0)} />
+        <Operation value={"="} onClickOperation={() => handleEquals()} />
+        <Operation value={"/"} onClickOperation={() => handleOp(divide)} />
       </div>
     </main>
   );
